688a52a56c841576b4734e538b333f2cfccb86fd
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2024-02-27 20:47:40 -0300

    creacion docker

--- clientRss/src/Hook/useGetData.jsx
+++ clientRss/src/Hook/useGetData.jsx
@@ -1,10 +1,31 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const useGetData = async (URL) => {
-  try {
-    const articles = await (await axios.get(URL)).data
-    return articles
-  } catch (error) { console.log('error conexion con el servidor') }
+const useGetData = (URL, name) => {
+  const [state, setState] = useState({
+    name,
+    dato: null,
+    isloading: true
+  })
+
+  useEffect(() => getAxiosData(URL), [])
+
+  const getAxiosData = async (URL) => {
+    try {
+      console.log('entra')
+      const articles = await axios.get(URL)
+      if (!articles.data) return
+      console.log(articles.data)
+      setState({
+        isloading: false
+      })
+    } catch (error) { console.log('error conexion con el servidor') }
+  }
+  console.log(state)
+  return {
+    name: state.name,
+    data: state.dato
+  }
 }
 
 export default useGetData
