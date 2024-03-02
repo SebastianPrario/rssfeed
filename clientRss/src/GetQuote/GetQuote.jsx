3268334a722068debf6320631b729361bc760cdf
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2024-03-02 20:44:52 -0300

    se creo componente  reloj y cotizacion

---
+++ clientRss/src/GetQuote/GetQuote.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import useGetData from '../Hook/useGetData'
+import styles from './GetQuote.module.css'
+import Spinner from '../component/Spinner/Spinner'
+
+const GetQuote = () => {
+  const URL = 'https://api.bluelytics.com.ar/v2/latest'
+
+  const { data, isloading } = useGetData(URL, 1800000)
+
+  return (
+    isloading
+      ? ((<><p>Cargando ...</p><Spinner /></>))
+      : (
+        <div translate='no'>
+          <p className='text-white fs-4  mt-3 text-center'>Cotización dolar hoy</p>
+          <div className={styles.div}>
+            <div>
+              <p className='ms-2 text-white fs-3 text-center'>Blue:</p>
+              <p className='ms-2 text-white fs-4'>venta: $ {data.blue.value_sell}</p>
+              <p className='ms-2 text-white fs-4'>compra: $ {data.blue.value_buy}</p>
+            </div>
+            <div>
+              <p className='ms-2 text-white fs-3 text-center'>Oficial:</p>
+              <p className='ms-2 text-white fs-4'>venta: $ {data.oficial.value_sell}</p>
+              <p className='ms-2 text-white fs-4'>compra: $ {data.oficial.value_buy}</p>
+            </div>
+
+          </div>
+          <div>
+            <p className='ms-3 text-white fs-7 text-center'> última actualización: {data.last_update.slice(0, 19).split('T')[1]} - {data.last_update.slice(0, 19).split('T')[0]}</p>
+          </div>
+        </div>)
+
+  )
+}
+
+export default GetQuote
--- clientRss/src/component/Component3/Component3.jsx
+++ clientRss/src/component/Component3/Component3.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import WeatherComponent from '../../WeatherComponent/WeatherComponent';
+import React from 'react'
+import WeatherComponent from '../../WeatherComponent/WeatherComponent'
+import GetQuote from '../../GetQuote/GetQuote'
+import GetTime from '../../GetTime/GetTime'
 
 const Component3 = () => {
-    return( 
-        <div id="carouselExampleSlidesOnly" 
-         data-ride="carousel" data-interval="6000"
-         className="carousel slide " data-bs-ride="carousel">
-        <div className="carousel-inner">
-            <div className="carousel-item active">
-               <WeatherComponent/>
-            </div>
-            <div className="carousel-item text-center ">
-              <p className='fs-4 text-white'>PUBLICITE AQUI</p>
-            </div>
-            <div className="carousel-item text-center bg-white vh-100 ">
-               <p className='fs-4 text-primary'>PUBLICITE AQUI</p>
-            </div>
+  return (
+    <div
+      id='carouselExampleSlidesOnly'
+      data-ride='carousel' data-interval='6000'
+      className='carousel slide ' data-bs-ride='carousel'
+    >
+      <div className='carousel-inner'>
+        <div className='carousel-item active'>
+          <WeatherComponent />
         </div>
+        <div className='carousel-item text-center '>
+          <GetQuote />
+        </div>
+        <div className='carousel-item text-center bg-white vh-100 '>
+          <p className='fs-4 text-primary'>PUBLICITE AQUI</p>
+        </div>
+        <div className='carousel-item text-center bg-white vh-100 '>
+          <GetTime />
+        </div>
+      </div>
     </div>
-    )
+  )
 }
 
-   
+export default Component3
 
-
-export default Component3;
-
-
-{/* <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+{ /* <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
 <div className="carousel-inner">
     <WeatherComponent />
     <div className="carousel-item">
@@ -37,4 +40,4 @@ export default Component3;
          <p className='display-4 mt-4 text-dark'> Publicite<br></br> aqui</p>
     </div>
 </div>
-</div> */}
+</div> */ }
