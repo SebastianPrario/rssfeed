4a787ab9a3328e8a7406edad2a1bab14bca904cd
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2024-03-03 17:12:53 -0300

    fix carousel mobile

--- clientRss/src/component/Component3/Component3.jsx
+++ clientRss/src/component/Component3/Component3.jsx
@@ -1,33 +1,31 @@
-import React from 'react';
-import WeatherComponent from '../../WeatherComponent/WeatherComponent';
+import React from 'react'
+import WeatherComponent from '../../WeatherComponent/WeatherComponent'
 
 const Component3 = () => {
-    return( 
-        <div id="carouselExampleSlidesOnly" 
-         data-ride="carousel" data-interval="6000"
-         className="carousel slide " data-bs-ride="carousel">
-        <div className="carousel-inner">
-            <div className="carousel-item active">
-               <WeatherComponent/>
-            </div>
-            <div className="carousel-item text-center ">
-              <p className='fs-4 text-white'>PUBLICITE AQUI</p>
-            </div>
-            <div className="carousel-item text-center bg-white vh-100 ">
-               <p className='fs-4 text-primary'>PUBLICITE AQUI</p>
-            </div>
+  return (
+    <div
+      id='carouselExampleSlidesOnly'
+      data-ride='carousel' data-interval='6000' data-bs-touch='true'
+      className='carousel slide ' data-bs-ride='carousel'
+    >
+      <div className='carousel-inner'>
+        <div className='carousel-item active'>
+          <WeatherComponent />
         </div>
+        <div className='carousel-item text-center '>
+          <p className='fs-4 text-white'>PUBLICITE AQUI</p>
+        </div>
+        <div className='carousel-item text-center bg-white vh-100 '>
+          <p className='fs-4 text-primary'>PUBLICITE AQUI</p>
+        </div>
+      </div>
     </div>
-    )
+  )
 }
 
-   
+export default Component3
 
-
-export default Component3;
-
-
-{/* <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+{ /* <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
 <div className="carousel-inner">
     <WeatherComponent />
     <div className="carousel-item">
@@ -37,4 +35,4 @@ export default Component3;
          <p className='display-4 mt-4 text-dark'> Publicite<br></br> aqui</p>
     </div>
 </div>
-</div> */}
+</div> */ }
