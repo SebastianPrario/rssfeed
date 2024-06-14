1cf45146c53d01bb99e5c3a8d7effd74b655ce01
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2024-06-14 18:09:16 -0300

    correccion en rutas

--- clientRss/src/views/NavBar/components/PreferUser/index.jsx
+++ clientRss/src/views/NavBar/components/PreferUser/index.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import Styled from '../UserForm/styles'
-import { db } from './../../../../fireBase/FireStore.js'
+import { db } from '@fire/FireStore.js'
 import { collection, doc, addDoc, setDoc } from 'firebase/firestore'
 import { userContext } from './../../../../../context/user'
 
