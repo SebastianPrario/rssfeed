191f8df3608e87d727cd9250256f7e0b7eb20369
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2024-06-14 17:58:34 -0300

    correccion en rutas

--- clientRss/src/views/NavBar/components/UserForm/index.jsx
+++ clientRss/src/views/NavBar/components/UserForm/index.jsx
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form'
 import Styled from './styles'
-import { createUserWithEmail } from '../../../../fireBase/createUserWithEmail'
+import { createUserWithEmail } from '../../../../fireBase/createUserWithEmail.js'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
