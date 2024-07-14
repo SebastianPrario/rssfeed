e48dedf33878b89fcc4405bacf8f01819ad675e0
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2024-07-14 20:01:37 -0300

    correccion en build

--- server/src/controlers/getClimaControlers.js
+++ server/src/controlers/getClimaControlers.js
@@ -1,57 +1,5 @@
-const { unZip} = require ('../utils/getWeather.js')
-
-
-
-
-// setInterval( async () =>
-// {
-//     try{
-//     datos = await  unzip()
-//     console.log(datos)
-//     }catch (error) {console.log(error)}
-// }, 50000)
-
-
-
-const climaInfo = async (ciudad) => {
-    let datos = await  unZip()
-
-    try {
-    const nuevoarray = []
-    
-    const arrayData = datos ? datos.split('/ \r\n ') : ""
-    arrayData.forEach( elem => {
-        const datos =  elem.split(';');
-            nuevoarray.push({
-                ciudad: datos[0],
-                fecha: datos[1],
-                hora: datos[2],
-                estado: datos[3],
-                viento: datos[4],
-                temperatura: datos[5],
-                termica: datos[6],
-                presion: datos[9]
-            })
-    })
-    
-    // for (let index = 0; index < arrayData.length; index++) {
-    //     const datos =  arrayData[index].split(';');
-    //     console.log(datos)
-    //     nuevoarray.push({
-    //         ciudad: datos[0],
-    //         fecha: datos[1],
-    //         estado: datos[3],
-    //         viento: datos[4],
-    //         temperatura:datos[5],
-    //         termica: datos[6],
-    //         presion: datos[9]
-
-    //     })
-    // }
-    const response =  nuevoarray.filter (elem => elem.ciudad === ciudad.ciudad)
-    return response
-    }catch (error) {console.log (error)}
-   
+const climaInfo =  (ciudad) => {
+    return
 }
 
 module.exports = climaInfo
